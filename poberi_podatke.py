822092edcef48bdb08da3934e488a84fc8d4a43c
Gal Zmazek <gzmazek@gmail.com>
2022-11-07 19:49:55 +0100

    Koda za json, novi podatki

--- poberi_podatke.py
+++ poberi_podatke.py
@@ -1,17 +1,17 @@
 import requests
 import time
 
-MAKSIMALNA_STRAN = 923
+MAKSIMALNA_STRAN = 971
 CAS_MED_ZAJEMANJEM = 0
-# https://www.truecar.com/used-cars-for-sale/listings/?page=900
+ZNAMKA = "mercedes-benz"
 
 for i in range(1, MAKSIMALNA_STRAN):
     time.sleep(CAS_MED_ZAJEMANJEM)
-    url = f"https://www.truecar.com/used-cars-for-sale/listings/bmw/?page={i}"
+    url = f"https://www.truecar.com/used-cars-for-sale/listings/{ZNAMKA}/?page={i}"
     odziv = requests.get(url)
     if odziv.status_code == 200:
         print(url)
-        with open(f"cars_data_2/bmw_page_{i}.html", "w") as f:
+        with open(f"cars_data_{ZNAMKA}/{ZNAMKA}_page_{i}.html", "w") as f:
             f.write(odziv.text)
     else:
         print("Prišlo je do napake")
--- preberi_podatke.py
+++ preberi_podatke.py
@@ -1,4 +1,15 @@
 import re
+import json
+import csv
+
+#######################################
+
+ZNAMKA_STRANI = [("mercedes-benz", 971), ("bmw", 918), ("audi", 508)]
+ZNAMKA_STRANI_POSKUS = [("mercedes-benz", 90), ("bmw", 91), ("audi", 50)]
+
+pravilnih = 0
+
+#######################################
 
 vzorec_bloka = re.compile(
     r'<li\sclass="mt(?P<blok>.+?)'
@@ -7,8 +18,7 @@ vzorec_bloka = re.compile(
 )
 
 vzorec_oglasa = re.compile(
-    r'<a\shref="/used-cars-for-sale/listing/(?P<vse>.*?)/".*'
-#    r'aria-label=(?P<naslov>)data-test="usedListing"'
+    r'<a\shref="/used-cars-for-sale/listing/(?P<primary>.*?)/".*'
     r'data-test="vehicleCardPricingBlockPrice">(?P<price>.*?)</div>.*'
     r'</circle>\s*</svg>(?P<miles>.*?)<!--\s-->\smiles</div>.*'
     r'</path></svg>(?P<city>.*?)<!--\s-->,\s<!--\s-->(?P<state>.*?)</div><div\sclass="vehicle-card-location.*'
@@ -19,24 +29,79 @@ vzorec_oglasa = re.compile(
     flags=re.DOTALL
 )
 
+#####################################################################################################################
+########################################   FUNKCIJE ZA ČIŠČENJE PODATKOV     ########################################
+#####################################################################################################################
 
-count = 0
-for i in range(1, 920):
-    with open(f"cars_data_2/bmw_page_{i}.html") as f:
-        vsebina = f.read()
-    for blok in vzorec_bloka.finditer(vsebina):
-        try:
-#            print(blok.group(0)[-1000:])
-            print(vzorec_oglasa.search(blok.group(0)).groupdict())
-            count += 1
-            print(count, i)
-        except:
-            pass
+def vzorec_primary(znamka):
+    return re.compile(
+        fr'(?P<VIN>.+)/(?P<year>\d{{4}})-{znamka}-(?P<model>.*)',
+        flags=re.DOTALL
+    )
 
-print(count)
+def predelaj_primary(primary, znamka):
+    return vzorec_primary(znamka).search(primary).groupdict()
 
-#    r'</path></svg>(?P<MPG>.*?)Engine:\s(?P<engine>.*?),\sTransmission:\s(?P<transmission>.*?)</div>.*'
-#    r'</path></svg>(?P<general>.*?)</div><div\s
-#    r'class="vehicle-card-vin-carousel\stext-xs">VIN:\s<!--\s-->(?P<VIN>.*?)</div></div></div></div></li><li.*'
+def predelaj_podatki_motor(podatki):
+    """Funkcija vzame niz podatkov in vrne ločene podatke, porabo vrne 0, če podatka ni"""
+    if "MPG" not in podatki:
+        podatki = "MPG: 0-0, " + podatki
+    slovar_podatki = re.search(r'MPG:\s(?P<MPG_city>\d+)-(?P<MPG_highway>\d+),\sEngine:\s(?P<engine>.*),\sTransmission:\s(?P<transmission>.*),\s(?P<drive_type>.*)', podatki).groupdict()
+    return slovar_podatki
+        
 
 
+#####################################################################################################################
+#####################################################################################################################
+
+def predelaj_podatke_oglasa(oglas, znamka):
+    try: # Ker so nekateri oglasi nepopolni (brez porabe, ki je knjična za analizo), so filtrirani le oglasi, ki vsebujejo potrebne informacije) - nepravilnih je približno 42%
+        avto = vzorec_oglasa.search(oglas.group(0)).groupdict()
+        osnovni_podatki = predelaj_primary(avto.pop("primary"), znamka)
+        avto["VIN"] = osnovni_podatki["VIN"]
+        avto["year"] = int(osnovni_podatki["year"])
+        avto["model"] = osnovni_podatki["model"]
+        avto["price"] = int(avto["price"].lstrip("$").replace(",", ""))
+        avto["miles"] = int(avto["miles"].replace(",", ""))
+        avto["accidents"] = int(avto["accidents"].replace("No", "0"))
+        avto["owners"] = int(avto["owners"])
+        motorni = predelaj_podatki_motor(avto.pop("podatki"))
+        avto["MPG_city"] = motorni["MPG_city"]
+        avto["MPG_highway"] = motorni["MPG_highway"]
+        avto["engine"] = motorni["engine"]
+        avto["transmission"] = motorni["transmission"]
+        avto["drive_type"] = motorni["drive_type"]
+
+        #print(avto["podatki"])
+        global pravilnih 
+        pravilnih += 1
+        return avto
+    except: 
+        pass
+
+#####################################################################################################################
+#####################################################################################################################
+
+cars = []
+
+for ZNAMKA, STRANI in ZNAMKA_STRANI_POSKUS:
+    for i in range(1, STRANI):
+        with open(f"cars_data_{ZNAMKA}/{ZNAMKA}_page_{i}.html") as f:
+            vsebina = f.read()
+        for blok in vzorec_bloka.finditer(vsebina):
+            car = predelaj_podatke_oglasa(blok, ZNAMKA)
+            if car:
+                cars.append(car)
+
+with open("cars.json", "w") as f:
+    json.dump(cars, f, ensure_ascii=False, indent=4)
+
+# with open("cars.csv", "w") as f:
+#    pisatelj = csv.DictWriter(f, fieldnames=[])
+#    pisatelj.writeheader()
+#    for car in cars:
+#        pisatelj.writerow(car)
+
+
+# halo = "mercesed-benz"
+# print(re.search(fr'(?P<VIN>.+)/(?P<year>\d{{4}})-{halo}-(?P<model>.*)', 'WA1ANAFYXK2079056/2019-audi-q5').groupdict()["year"])
